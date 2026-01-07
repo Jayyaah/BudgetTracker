ad4f553400eb653f39a0035834c302ad0716f08e
Jayyaah <valentines.maitre@gmail.com>
2026-01-07 17:38:10 +0100

    Initial Commit

---
+++ BudgetTracker/BudgetTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetTrackerApp.swift
+//  BudgetTracker
+//
+//  Created by Valentine on 07/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct BudgetTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetTrackerTests/BudgetTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetTrackerTests.swift
+//  BudgetTrackerTests
+//
+//  Created by Valentine on 07/01/2026.
+//
+
+import Testing
+@testable import BudgetTracker
+
+struct BudgetTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
