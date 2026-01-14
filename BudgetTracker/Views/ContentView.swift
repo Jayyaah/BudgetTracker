91d8741ca0c45f980b798adfe21c71182c8ffc83
Valentine <38808380+Jayyaah@users.noreply.github.com>
2026-01-14 16:13:35 +0100

    Update ContentView.swift

--- BudgetTracker/Views/ContentView.swift
+++ BudgetTracker/Views/ContentView.swift
@@ -8,12 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var viewModel = BudgetViewModel()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            DashboardView(viewModel: viewModel)
         }
         .padding()
     }
